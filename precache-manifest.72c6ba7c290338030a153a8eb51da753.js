b423a96dc07d9600bcbb973aa6914f1e814c1005
MutiahAI <apampa.mutiah@gmail.com>
2024-04-26 23:31:39 +0100

    Updates

---
+++ precache-manifest.72c6ba7c290338030a153a8eb51da753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fd7dde93dc976ed5e0b89299ca7ad3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b47e2842bd049ba540e",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6223a0d8851caa4d3a19",
+    "url": "/static/css/main.7dda1a76.chunk.css"
+  },
+  {
+    "revision": "4b47e2842bd049ba540e",
+    "url": "/static/js/2.2219979c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.2219979c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6223a0d8851caa4d3a19",
+    "url": "/static/js/main.cb7e2122.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
